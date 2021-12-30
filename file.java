5a507b708e6a5bc1b193a5f7eb69b687c310b14a
Sebastian Barry <dev@mmbsoft.com>
2021-12-29 22:07:04 -0500

    update to coc

---
+++ file.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class ThisClass {
+
+    int main; 
+    int value;
+
+    public ThisClass(
+            int main, 
+            int value,
+            ) {
+        self.main = main;
+        self.value = value;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    public static int getValue() {
+        List<Integer> list = new ArrayList<>();
+        return self.value; 
+    }
+}
+
